704f7c632bfa617d1e5e40ae1ab23431dcbe9477
kryzkl <46037287+korayozkal@users.noreply.github.com>
2020-10-19 15:27:21 -0700

    destroy_all methods added to start clean seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Comicbook.destroy_all
+Publisher.destroy_all
+
 marvel = Publisher.create(name: "Marvel Comics")
 dc = Publisher.create(name: "DC Comics")
 image= Publisher.create(name: "Image Comics")
