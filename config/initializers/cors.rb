e275503fdd5c254a32bf58f11c3fc832904974a3
kryzkl <46037287+korayozkal@users.noreply.github.com>
2020-10-19 15:43:39 -0700

    gem 'rack-cors' added and cors.rb edited

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,17 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+ allow do
+    origins '*'
+
+    resource '*',
+       headers: :any,
+       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+   end
+ end
+
+# NOTES TO MYSELF =>  Rails provides the cors.rb file specifically for defining our rules for cross-origin HTTP requests.
+# The string after origins specifies which hosts will be allowed to make requests to your API.
+# Your brand-new rails app probably comes with the string "example.com" there — change it to "*" to allow all hosts.
+
