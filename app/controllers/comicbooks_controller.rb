1c1031e71f3e7594ffac9804d1a24ac8510702a4
kryzkl <46037287+korayozkal@users.noreply.github.com>
2020-10-23 22:08:15 -0700

    Class name error on Create action corrected

--- app/controllers/comicbooks_controller.rb
+++ app/controllers/comicbooks_controller.rb
@@ -9,13 +9,13 @@ class ComicbooksController < ApplicationController
 
 
     def create
-        comicbook = comicbooks.new(comicbook_params)
-      if @comicbook.save
+       #byebug
+        comicbook = Comicbook.new(comicbook_params)
+        #byebug
+      if comicbook.save
         render json: comicbook, status: :accepted
-        #https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/202
       else
-        render json: {errors: syllabus.errors.full_messages}, status: :unprocessible_entity
-        #https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/422
+        render json: {errors: comicbook.errors.full_messages}, status: :unprocessible_entity
       end 
     end 
 
@@ -23,8 +23,5 @@ private
     def comicbook_params
        params.require(:comicbook).permit(:title, :number, :writer, :artist, :publisher_id, :description, :image_url)
     end
-
-
-
 
 end
